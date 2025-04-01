88da868040622abace68397bf57d0e1217f1e21e
donggun <hugh3232@naver.com>
2025-04-02 08:34:12 +0900

    chore : controller 수정

--- src/domain/auth/auth.module.ts
+++ src/domain/auth/auth.module.ts
@@ -4,13 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthService } from './service/auth.service';
 
 
-
 @Module({
     imports: [
       ConfigModule.forRoot(), 
     ],
-    providers: [AuthService,AuthController],
-    controllers: [AuthController],
-    exports: [AuthService]
+    providers: [AuthService],  //주로 서비스(@Injectable())나 리포지토리를 등록하는 배열입니다.
+    controllers: [AuthController], 
+    exports: [AuthService] //컨트롤러는 모듈 외부에서 직접 주입하여 사용하지 않으므로, exports할 필요가 없습니다.
   })
   export class AuthModule { }
+  
